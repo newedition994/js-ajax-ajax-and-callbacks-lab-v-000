8366096627431ddf18ae4030ecb375c89f08c1ad
Breyanna Vass <breyanna.bvass@gmail.com>
2018-08-30 18:02:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.");
+
+var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+    $('#results').html(renderSearchResults(data))
+  }).fail(error => {
+    displayError()
+  });
+}
+
+
 $(document).ready(function (){
 });
